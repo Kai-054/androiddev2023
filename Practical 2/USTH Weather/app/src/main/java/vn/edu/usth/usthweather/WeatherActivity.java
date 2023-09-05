6ac068e6d66abdf5d3f7b84b4c32b0cc7b6525ee
Daniel Kai <124031177+Kai-054@users.noreply.github.com>
2023-09-06 00:07:21 +0700

    update code for lab 4,5,6

---
+++ Practical 2/USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,51 @@
+package vn.edu.usth.weather;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static String TAG = "WeatherActivity";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate: This is a log message.");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onResume: This is a log message.");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: This is a log message.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: This is a log message.");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onResume: This is a log message.");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onResume: This is a log message.");
+    }
+}
+
+
+
+
+
+
